e9ee1a321fcae834bea9cdd03084eda0853f796e
miiiii-ide <midegami.work@gmail.com>
2020-08-20 20:38:49 +0900

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { Store } from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    count: 10,
+    amount: 100,
+    todos: [
+      {id: 1, text: '...', done: true},
+      {id: 2, text: '...', done: false},
+    ]
+  },
+  getters:{
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    incrementPayload(state, payload) {
+      state.amount += payload.amount
+    }
+  },
   actions: {},
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    {{count}}
+    <button @click="increment">+</button>
+    <hr />
+    {{amount}}
+    <button @click="incrementPayload">+</button>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent, computed } from "@vue/composition-api";
 
-export default {
+export default defineComponent({
   name: "Home",
   components: {
     HelloWorld
+  },
+  setup(props, { root }) {
+    // const state = {
+    //   count: computed(() => root.$store.state.count)
+    // };
+    const count = computed(() => root.$store.state.count);
+    const amount = computed(() => root.$store.state.amount);
+    
+    function increment() {
+      root.$store.commit("increment");
+    }
+
+    function incrementPayload() {
+      root.$store.commit("incrementPayload", { amount: 100 });
+    }
+
+    return {
+      // state,
+      count,
+      amount,
+      increment,
+      incrementPayload
+    };
   }
-};
+});
 </script>
